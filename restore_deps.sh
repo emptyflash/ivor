2348288cc3e43b45a600a17564737d7eec2048ed
Cameron Alexander <emptyflash@gmail.com>
2017-10-19 13:10:01 -0500

    Actually I don't want those script commited

--- restore_deps.sh
+++
@@ -1,3 +0,0 @@
-mv $(idris --libdir)/tomladris_ $(idris --libdir)/tomladris
-mv $(idris --libdir)/lightyear_ $(idris --libdir)/lightyear
-mv $(idris --libdir)/ipkgparser_ $(idris --libdir)/ipkgparser
